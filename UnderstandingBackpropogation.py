47ff6cfe0af95db4fe14b59351e7d744b4920264
Jon Xu <73413275+jonxu88@users.noreply.github.com>
2020-11-22 01:35:42 +0900

    Add files via upload

---
+++ UnderstandingBackpropogation.py
@@ -0,0 +1,32 @@
+#Following https://www.youtube.com/watch?v=8d6jf7s6_Qs
+# I coded the backpropogation for the simplest neural network.
+#There is only one input layer (with one neuron),
+# one output layer with one neuron, and no bias or activation function.
+
+print("-----")
+print('Enter the number of iterations:') #what is the machine learning term for 'number of iterations?'
+iterations = input()
+iterations = int(iterations)
+
+print("-----")
+print('Enter the learning rate:')
+learning_rate = input()
+learning_rate = float(learning_rate)
+
+print("------")
+print('Enter the starting weight:') #usually this is randomized?
+weight = input()
+weight = float(learning_rate)
+
+inputlayer = 1.5
+desired_output = 0.5
+
+
+
+for i in range(iterations):
+    dC_dw = inputlayer * 2 * (inputlayer * weight  - desired_output)
+    weight = weight - learning_rate * dC_dw
+    cost = (inputlayer * weight - desired_output)**2
+    print('Iteration Number: ' + str(i+1) + ', Weight:' + str(weight) + ', Cost:' + str(cost))
+    
+
