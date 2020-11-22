64cc6eb50eab0fd17a8121e98fb0eed71d6bedd1
Jon Xu <73413275+jonxu88@users.noreply.github.com>
2020-11-23 00:20:30 +0900

    Add files via upload

--- UnderstandingBackpropogation.py
+++ UnderstandingBackpropogation.py
@@ -4,6 +4,21 @@
 # one output layer with one neuron, and no bias or activation function.
 
 print("-----")
+print('Enter the input sample (set to 1.5 in the video):')
+inputlayer = input()
+inputlayer = float(inputlayer)
+
+print("-----")
+print('Enter the desired output (set to 0.5 in the video):')
+desired_output = input()
+desired_output = float(desired_output)
+
+print("------")
+print('Enter the starting weight (set to 0.8 in the video):') #usually this is randomized?
+weight = input()
+weight = float(weight) #in the video this is set to be 0.8 I believe
+
+print("-----")
 print('Enter the number of iterations:') #what is the machine learning term for 'number of iterations?'
 iterations = input()
 iterations = int(iterations)
@@ -12,15 +27,6 @@ print("-----")
 print('Enter the learning rate:')
 learning_rate = input()
 learning_rate = float(learning_rate)
-
-print("------")
-print('Enter the starting weight:') #usually this is randomized?
-weight = input()
-weight = float(weight) #in the video this is set to be 0.8 I believe
-
-inputlayer = 1.5 #Can also edit to change these if desired
-desired_output = 0.5
-
 
 
 for i in range(iterations):
